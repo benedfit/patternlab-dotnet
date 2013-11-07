f7dd8a778cdb9396069edf12282e96b9a4f118e5
unknown <Ben.Edwards@LDNMRM-LW71641.emea.corp.ipgnetwork.com>
2013-11-07 09:57:21 +0000

    Currently broken as I've moved PatternLab into an Area and it's now only half working

---
+++ PatternLab/Areas/Styleguide/Controllers/StyleguideController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace PatternLab.Areas.Styleguide.Controllers
+{
+    public class StyleguideController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PatternLab/Areas/Styleguide/StyleguideAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PatternLab.Areas.Styleguide
+{
+    public class StyleguideAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Styleguide";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Styleguide_default",
+                "Styleguide/{controller}/{action}/{id}",
+                new { controller = "Styleguide", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
