dc295910975a3ddf14cf0c002530aaabed2f71f4
unknown <ben.edwards@LDNMRM-LW71641.emea.corp.ipgnetwork.com>
2013-11-08 13:22:56 +0000

    Layout view optimisation

--- PatternLab/Areas/Styleguide/Controllers/HtmlController.cs
+++ PatternLab/Areas/Styleguide/Controllers/HtmlController.cs
@@ -1,17 +1,25 @@
 ﻿using System.Web.Mvc;
+using PatternLab.Areas.Styleguide.Providers;
 
 namespace PatternLab.Areas.Styleguide.Controllers
 {
     public class HtmlController : Controller
     {
+        private IPatternProvider _provider;
+
+        public HtmlController()
+        {
+            _provider = new PatternProvider();
+        }
+
         public ActionResult All()
         {
-            return View();
+            return View(_provider.Patterns());
         }
 
         public ActionResult Index()
         {
-            return View();
+            return View(_provider.Patterns());
         }
     }
 }
