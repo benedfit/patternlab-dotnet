357d16696ff5c6f77b3bd4c7459e4511091b17c1
benedfit <hello@benedfit.com>
2014-04-15 09:49:02 +0100

    Moved handler registration to code behind

--- PatternLab.Core/Modules/PatternLabModule.cs
+++ PatternLab.Core/Modules/PatternLabModule.cs
@@ -1,4 +1,8 @@
-﻿using System.Web;
+﻿using System.Configuration;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Configuration;
 using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -18,8 +22,6 @@ namespace PatternLab.Core.Modules
 
         public void Init(HttpApplication context)
         {
-            //context.PostMapRequestHandler += context_PostMapRequestHandler;
-
             HostingEnvironment.RegisterVirtualPathProvider(new EmbeddedResourceProvider());
 
             AreaRegistration.RegisterAllAreas();
@@ -29,21 +31,97 @@ namespace PatternLab.Core.Modules
             ViewEngines.Engines.Add(new MustaceViewEngine());
         }
 
-        /*private static void context_PostMapRequestHandler(object sender, System.EventArgs e)
-        {
-            //TODO: #2 Create config system that doesn't rely on appSettings in web.config
-            var context = new HttpContextWrapper(((HttpApplication)sender).Context);
-            var extensions = new[] {".css", ".jss"};
-
-            if (context.Request.Url != null && context.Request.Url.PathAndQuery.Contains(".css"))
-            {
-                context.Response.Write(context.Request.Url);
-            }
-        }*/
-
         public static void LoadModule()
         {
             DynamicModuleUtility.RegisterModule(typeof (PatternLabModule));
+
+            RegisterHttpHandler("PatternLabStyleguide", "styleguide/*", "*", "System.Web.StaticFileHandler");
+            RegisterHttpHandler("PatternLabPatterns", "patterns/*/*.html", "*", "System.Web.StaticFileHandler");
+        }
+
+        private static void RegisterHttpHandler(string name, string path, string verb, string type)
+        {
+            var configuration = WebConfigurationManager.OpenWebConfiguration("~");
+            var section = (IgnoreSection) configuration.GetSection("system.webServer");
+            var xml = section.SectionInformation.GetRawXml();
+            var save = false;
+            
+            if (xml == null)
+            {
+                xml =
+                    string.Format(
+                        "<system.webServer><handlers><add name=\"{0}\" path=\"{1}\" verb=\"{2}\" type=\"{3}\" /></handlers></system.webServer>",
+                        name, path, verb, type);
+                save = true;
+            }
+            else if (xml.Contains(name))
+            {
+                return;
+            }
+            else
+            {
+                xml =
+                    Regex.Replace(
+                        Regex.Replace(
+                            Regex.Replace(
+                                Regex.Replace(
+                                    Regex.Replace(Regex.Replace(xml, @"<handlers\s*/>", "<handlers/>"),
+                                        @"</handlers\s*>", "</handlers>"), @"<handlers\s*>", "<handlers>"),
+                                @"<system.webServer\s*>", "<system.webServer>"), @"<system.webServer\s*/>",
+                            "<system.webServer/>"), @"</system.webServer\s*>", "</system.webServer>");
+                if (xml.Contains("<handlers/>"))
+                {
+                    xml = xml.Replace("<handlers/>",
+                        string.Format(
+                            "<handlers><add name=\"{0}\" path=\"{1}\" verb=\"{2}\" type=\"{3}\" /></handlers>", name,
+                            path, verb, type));
+                    save = true;
+                }
+                else if (xml.Contains("<handlers"))
+                {
+                    var add = string.Format("<add name=\"{0}\" path=\"{1}\" verb=\"{2}\" type=\"{3}\"", name, path,
+                        verb, type);
+                    if (!xml.Contains(add))
+                    {
+                        add = add + "/>";
+                        var index = xml.IndexOf("</handlers>", System.StringComparison.OrdinalIgnoreCase);
+
+                        var builder = new StringBuilder(xml);
+                        builder.Insert(index, add, 1);
+                        xml = builder.ToString();
+
+                        save = true;
+                    }
+                }
+                else if (xml.Contains("<system.webServer/>"))
+                {
+                    var add =
+                        string.Format(
+                            "<system.webServer><handlers><add name=\"{0}\" path=\"{1}\" verb=\"{2}\" type=\"{3}\" /></handlers></system.webServer>",
+                            name, path, verb, type);
+                    xml = xml.Replace("<system.webServer/>", add);
+                    save = true;
+                }
+                else if (xml.Contains("<system.webServer"))
+                {
+                    var add =
+                        string.Format(
+                            "<handlers><add name=\"{0}\" path=\"{1}\" verb=\"{2}\" type=\"{3}\" /></handlers>", name,
+                            path, verb, type);
+                    var index = xml.IndexOf("</system.webServer>", System.StringComparison.OrdinalIgnoreCase);
+
+                    var builder = new StringBuilder(xml);
+                    builder.Insert(index, add, 1);
+                    xml = builder.ToString();
+
+                    save = true;
+                }
+            }
+
+            if (!save) return;
+
+            section.SectionInformation.SetRawXml(xml);
+            configuration.Save();
         }
 
         private static void RegisterRoutes(RouteCollection routes)
@@ -51,7 +129,7 @@ namespace PatternLab.Core.Modules
             routes.Clear();
 
             routes.Add("PatternLabAsset", new Route("{root}/{*path}", new RouteValueDictionary(new {}),
-                new RouteValueDictionary(new {root = "Styleguide", path = @"^(?!html).+"}),
+                new RouteValueDictionary(new {root = "styleguide", path = @"^(?!html).+"}),
                 new EmbeddedResourceRouteHandler()));
 
             routes.MapRoute("PatternLabStyleguide", "styleguide/html/styleguide.html",
