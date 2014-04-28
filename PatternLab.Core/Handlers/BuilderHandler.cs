fb4b891f4eca73fd7e36d39321adc7fd6f98f7fc
Ben Edwards <hello@benedfit.com>
2014-04-28 20:17:59 +0100

    Work towards #19

--- PatternLab.Core/Handlers/BuilderHandler.cs
+++ PatternLab.Core/Handlers/BuilderHandler.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Routing;
 using Abot.Crawler;
 using Abot.Poco;
-using CsQuery.ExtensionMethods;
-using CsQuery.Implementation;
-using Nustache.Core;
 using PatternLab.Core.Providers;
 
 namespace PatternLab.Core.Handlers
 {
     public class BuilderHandler : IHttpHandler
     {
-        private readonly RouteData _routeData;
+        private readonly HttpContextBase _httpContext;
+        private readonly string _path;
 
-        public BuilderHandler(RouteData routeData)
+        public BuilderHandler(RouteData routeData, HttpContextBase httpContext)
         {
-            _routeData = routeData;
+            var routeDataValue = routeData.Values;
+
+            _httpContext = httpContext;
+            _path = routeDataValue["path"] != null
+                ? routeDataValue["path"].ToString()
+                : string.Empty;
+            if (string.IsNullOrEmpty(_path))
+            {
+                _path = PatternProvider.FolderNameBuilder;
+            }
         }
 
         public bool IsReusable
@@ -32,30 +38,21 @@ namespace PatternLab.Core.Handlers
             // TODO: #19 Create static output generator
             context.Response.Clear();
 
-            var routeDataValues = _routeData.Values;
-            var isSnapshot = true;
-
-            var path = routeDataValues["path"] != null
-                ? routeDataValues["path"].ToString()
-                : string.Empty;
-            if (string.IsNullOrEmpty(path))
-            {
-                path = PatternProvider.FolderNameBuilder;
-                isSnapshot = false;
-            }
-            path = Path.Combine(HttpRuntime.AppDomainAppPath, path);
-
             var start = DateTime.Now;
             var memory = 0;
 
             var message = new StringBuilder();
             message.Append("configuring pattern lab...<br/>");
 
-            var crawlConfig = new CrawlConfiguration {MaxConcurrentThreads = 20, DownloadableContentTypes = "text/html"};
-            
+            var crawlConfig = new CrawlConfiguration
+            {
+                MaxConcurrentThreads = 20,
+                DownloadableContentTypes = "text/html, text/plain"
+            };
+
             var crawler = new PoliteWebCrawler(crawlConfig, null, null, null, null, null, null, null, null);
-            crawler.PageCrawlCompletedAsync += crawler_PageCrawlCompletedAsync;
-            
+            crawler.PageCrawlCompletedAsync += PageCrawlCompletedAsync;
+
             var crawlerPath = string.Format("{0}://{1}{2}{3}",
                 context.Request.Url.Scheme,
                 context.Request.Url.Host,
@@ -69,51 +66,26 @@ namespace PatternLab.Core.Handlers
             }
 
             var result = crawler.Crawl(new Uri(crawlerPath));
-
-            
-            
-            /*var provider = Controllers.PatternLabController.Provider ?? new PatternProvider();
-            var data = provider.Data();
-            var patterns = provider.Patterns().Where(p => !p.Hidden).ToList();
-
-            foreach (var pattern in patterns)
-            {
-                var patternFilePath = Path.Combine(path, "patterns", pattern.HtmlUrl);
-                var patternFolderName = Path.GetDirectoryName(patternFilePath);
-
-                if (patternFolderName != null)
-                {
-                    Directory.CreateDirectory(patternFolderName);
-                }
-
-                Render.StringToFile(pattern.Html, data, patternFilePath);
-            }*/
-
             var elapsed = DateTime.Now - start;
 
             message.Append("your site has been generated...<br/>");
-            if (isSnapshot)
-            {
-                message.AppendFormat("output directory: {0}<br />", path);
-            }
-            message.AppendFormat("site generation took {0} seconds and used {1}MB of memory...<br/>", elapsed.TotalSeconds, memory);
+            message.AppendFormat("site generation took {0} seconds and used {1}MB of memory...<br/>",
+                elapsed.TotalSeconds, memory);
 
             context.Response.Write(message.ToString());
         }
 
-        void crawler_PageCrawlCompletedAsync(object sender, PageCrawlCompletedArgs e)
+        private void PageCrawlCompletedAsync(object sender, PageCrawlCompletedArgs e)
         {
             var crawledPage = e.CrawledPage;
-            var fileName = crawledPage.Uri.AbsolutePath;
-            if (fileName.EndsWith("/"))
+            var virtualPath = crawledPage.Uri.AbsolutePath;
+            if (virtualPath.EndsWith("/"))
             {
-                fileName = string.Concat(fileName, "index.html");
+                virtualPath = string.Concat(virtualPath, "index.html");
             }
-            if (fileName.StartsWith("/"))
-            {
-                fileName = fileName.TrimStart('/');
-            }
-            var filePath = Path.Combine(PatternProvider.FolderNameBuilder, fileName);
+            var filePath = _httpContext.Server.MapPath(virtualPath);
+            filePath = filePath.Replace(HttpRuntime.AppDomainAppPath,
+                string.Format("{0}{1}\\", HttpRuntime.AppDomainAppPath, _path));
             var folderName = Path.GetDirectoryName(filePath);
 
             if (folderName != null)
@@ -121,7 +93,7 @@ namespace PatternLab.Core.Handlers
                 Directory.CreateDirectory(folderName);
             }
 
-            Render.StringToFile(crawledPage.Content.Text, null, Path.Combine(HttpRuntime.AppDomainAppPath, filePath));
+            File.WriteAllText(filePath, crawledPage.Content.Text);
         }
     }
 
@@ -129,7 +101,7 @@ namespace PatternLab.Core.Handlers
     {
         public IHttpHandler GetHttpHandler(RequestContext requestContext)
         {
-            return new BuilderHandler(requestContext.RouteData);
+            return new BuilderHandler(requestContext.RouteData, requestContext.HttpContext);
         }
     }
 }
