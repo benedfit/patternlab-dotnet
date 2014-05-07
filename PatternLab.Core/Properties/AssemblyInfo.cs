44a2ce3aa7120c265500e35dff9ba43453442cf6
benedfit <hello@benedfit.com>
2014-05-07 15:53:13 +0100

    Restructure of solution for nuget release #7

--- PatternLab.Core/Properties/AssemblyInfo.cs
+++ PatternLab.Core/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("0.7.12")]
 [assembly: AssemblyTitle("PatternLab.Core")]
 [assembly: AssemblyProduct("Pattern Lab")]
-[assembly: AssemblyDescription("The Pattern Lab core assemblies")]
+[assembly: AssemblyDescription("The core assemblies for the .NET version of Pattern Lab")]
 [assembly: AssemblyCopyright("Copyright (c) 2013-14 Brad Frost, http://bradfrostweb.com, Dave Olsen, http://dmolsen.com & Ben Edwards, http://benedfit.com")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ef5cf2bd-c020-460b-bd00-7cf248d73ea1")]
