fb1fb87f95b7f8b73ca4d65367918befe94620ef
benedfit <hello@benedfit.com>
2014-05-09 17:04:21 +0100

    Added missing Global.asax.cs to Nuget package

---
+++ PatternLab/Global.asax.cs.pp
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using PatternLab.Core;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Starter kit Pattern Lab application class
+    /// </summary>
+    public class Application : Global
+    {
+        /// <summary>
+        /// The event that fires at application start
+        /// </summary>
+        public void Application_Start()
+        {
+            // Register any configured Areas
+            AreaRegistration.RegisterAllAreas();
+        }
+    }
+}
