6bb94e806af76df06f473ed94bb53e21311c1b20
benedfit <hello@benedfit.com>
2014-05-28 12:26:03 +0100

    Razor support progressing

--- PatternLab/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// Project specific assembly information
-[assembly: AssemblyTitle("Pattern Lab (Mustache)")]
-[assembly: AssemblyDescription(".NET version of Pattern Lab (Mustache)")]
-[assembly: ComVisible(false)]
-[assembly: Guid("a12a3388-6f19-4caa-ac8b-4e46927781ea")]
--- PatternLab/Properties/AssemblyInfoCommon.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-// Shared assembly information
-[assembly: AssemblyProduct("Pattern Lab")]
-[assembly: AssemblyFileVersion("0.8.0")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyCompanyAttribute("Brad Frost,Dave Olsen,Ben Edwards")]
-[assembly: AssemblyCopyright("Copyright (c) 2013-14 Brad Frost, http://bradfrostweb.com, Dave Olsen, http://dmolsen.com & Ben Edwards, http://benedfit.com")]
